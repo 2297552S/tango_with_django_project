e4a66156b6110e6f3360a27990af57daffa81547
Henry Speir <2297552s@student.gla.ac.uk>
2020-02-14 15:51:47 +0000

    Passes Chapter 7 & 8

--- rango/views.py
+++ rango/views.py
@@ -99,7 +99,7 @@ def show_category(request, category_name_slug):
     # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context=context_dict)
 
-@login_required
+#@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -124,7 +124,7 @@ def add_category(request):
     
     return render(request, 'rango/add_category.html', {'form': form})
 
-@login_required
+#@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
