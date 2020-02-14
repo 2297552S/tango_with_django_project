e78600d79ac70ee943d4ec93ebec45a06438b897
Henry Speir <2297552s@student.gla.ac.uk>
2020-02-14 15:15:20 +0000

    Chapter 10: complete

--- rango/views.py
+++ rango/views.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+from datetime import datetime
+
 # Import the models
 from rango.models import Category,Page
 
@@ -25,14 +27,46 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
-    # Render the response and send it back!
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+
+    # Obtain our Response object early so we can add cookie information.
+    response = render(request, 'rango/index.html', context=context_dict)
+    # Return response back to the user, updating any cookies that need changed.
+    return response
 
 def about(request):
 
     context_dict = {'boldmessage': 'This tutorial has been put together by Henry Speir'}
 
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    
     return render(request, 'rango/about.html', context= context_dict)
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val =None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+# Updated the function definition
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request,'visits','1'))
+    last_visit_cookie = get_server_side_cookie(request,'last_visit',str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+
+    #Update/set the visits cookie
+    request.session['visits'] = visits
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass 
